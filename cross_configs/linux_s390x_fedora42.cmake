f745100d28690954ed0cc63a3a546d5fb52d80bb
Rot127 <unisono@quyllur.org>
2025-10-28 11:48:32 -0500

    Update fedora cross build script

--- cross_configs/linux_s390x_fedora42.cmake
+++ cross_configs/linux_s390x_fedora42.cmake
@@ -1,9 +1,8 @@
-# Bug of cmake not passing sysroot eary enough
-# https://stackoverflow.com/questions/36195791/cmake-missing-sysroot-when-cross-compiling
-#
 # This example file is for build on Fedora 42.
 # Search for required packages with `dnf search s390x`
 
+# Bug of cmake not passing sysroot early enough
+# https://stackoverflow.com/questions/36195791/cmake-missing-sysroot-when-cross-compiling
 set(CMAKE_C_COMPILE_OPTIONS_SYSROOT "--sysroot=")
 set(CMAKE_CXX_COMPILE_OPTIONS_SYSROOT "--sysroot=")
 
@@ -18,3 +17,4 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
+set(CMAKE_CROSSCOMPILING_EMULATOR "qemu-s390x-static;-L;/usr/s390x-redhat-linux/sys-root/fc42/usr/")
