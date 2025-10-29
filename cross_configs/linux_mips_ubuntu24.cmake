5c5ddcec234022ac66defccb769d036f19d516b9
Rot127 <unisono@quyllur.org>
2025-10-29 06:41:41 -0500

    Fast toolchain file fixes

--- cross_configs/linux_mips_ubuntu24.cmake
+++ cross_configs/linux_mips_ubuntu24.cmake
@@ -5,7 +5,8 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR mips)
 
 set(CMAKE_C_COMPILER   mips-linux-gnu-gcc)
-set(CMAKE_CXX_COMPILER mips-linux-gnu-g++)
+set(CMAKE_ASM_COMPILER mips-linux-gnu-gcc)
+set(CMAKE_CROSS_COMPILING 1)
 
 set(CMAKE_SYSROOT /usr/mips-linux-gnu/usr/)
 
--- cross_configs/linux_ppc64_ubuntu24.cmake
+++ cross_configs/linux_ppc64_ubuntu24.cmake
@@ -5,7 +5,8 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR ppc64)
 
 set(CMAKE_C_COMPILER   powerpc64-linux-gnu-gcc)
-set(CMAKE_CXX_COMPILER powerpc64-linux-gnu-g++)
+set(CMAKE_ASM_COMPILER powerpc64-linux-gnu-gcc)
+set(CMAKE_CROSS_COMPILING 1)
 
 set(CMAKE_SYSROOT /usr/powerpc64-linux-gnu/usr)
 
